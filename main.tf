5a53853a93ec92bd40e4282cb90e4de694d6c1bc
John Cena <John@gmail.com>
2021-02-05 00:01:21 -0700

    terraform network e.g

--- main.tf
+++ main.tf
@@ -7,16 +7,34 @@ provider "azurerm" {
   #version = "~> 2.37.0"
   features {}
 }
-resource "azurerm_resource_group" "example" {
-  name     = "azurevm"
-  location = "centralus"
+resource "azurerm_resource_group" "web_server_rg" {
+  name     = var.web_server_rg
+  location = var.web_server_location
 }
 #create a vnet
-resource "azurerm_virtual_network" "myterrafomrnetwork" {
-  name                = "virtualNetwork1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_virtual_network" "web_server_vnet" {
+  name                = "${var.resource_prefix}-vnet"
+  location            = var.web_server_location
+  resource_group_name = azurerm_resource_group.web_server_rg.name
   address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "web_server_subnet" {
+  name                = "${var.resource_prefix}-azure_subnet"
+  resource_group_name = azurerm_resource_group.web_server_rg.name
+  virtual_network_name = azurerm_virtual_network.web_server_vnet.name 
+  address_prefix = var.web_server_address_prefix
+}
+
+resource "azurerm_network_interface" "web_server_nic" {
+name = "$var{var.web_server_name}-nic"
+location = var.web_server_location
+resource_group_name = azurerm_resource_group.web_server_rg.name
+ ip_configuration {
+   name = "${var.web_server_name}-nic"
+   subnet_id = azurerm_subnet.web_server_subnet.id 
+   private_ip_address_allocation = "dynamic"
+ }
 }
 
 
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable web_server_rg {
+    type = string
+}
+variable web_server_location {
+    type = string
+}
+variable resource_prefix {
+    type = string
+}
+
+variable web_server_address_prefix {
+    type = string
+}
+
+variable web_server_name {
+    type = string
+}
