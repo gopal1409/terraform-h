c4b5734b7ec41dbdff1ffe9c8f7c855e3ef1c7b7
John Cena <John@gmail.com>
2021-02-05 00:28:19 -0700

    terraform with nic

--- main.tf
+++ main.tf
@@ -20,18 +20,18 @@ resource "azurerm_virtual_network" "web_server_vnet" {
 }
 
 resource "azurerm_subnet" "web_server_subnet" {
-  name                = "${var.resource_prefix}-azure_subnet"
+  name                = "${var.resource_prefix}-subnet"
   resource_group_name = azurerm_resource_group.web_server_rg.name
   virtual_network_name = azurerm_virtual_network.web_server_vnet.name 
-  address_prefix = var.web_server_address_prefix
+  address_prefixes = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "web_server_nic" {
-name = "$var{var.web_server_name}-nic"
+name = "${var.web_server_name}-nic"
 location = var.web_server_location
 resource_group_name = azurerm_resource_group.web_server_rg.name
  ip_configuration {
-   name = "${var.web_server_name}-nic"
+   name = "${var.web_server_name}-ip"
    subnet_id = azurerm_subnet.web_server_subnet.id 
    private_ip_address_allocation = "dynamic"
  }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
-variable web_server_rg {
+variable "web_server_rg" {
     type = string
 }
-variable web_server_location {
+variable "web_server_location" {
     type = string
 }
-variable resource_prefix {
-    type = string
-}
-
-variable web_server_address_prefix {
+variable "resource_prefix" {
     type = string
 }
 
-variable web_server_name {
+variable "web_server_address_prefix" {
+    type = string
+}
+
+variable "web_server_name" {
     type = string
 }
